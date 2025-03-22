609907391a79d3ecb0a53d68a8221a3aa533ddfe
SM7958302 <sm7958302@gmail.com>
2025-03-22 15:41:59 +0530

    chore: disable dev indicators in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  devIndicators: {
+    buildActivity: false,
+    buildActivityPosition: "bottom-right",
+  },
   eslint: {
-    ignoreDuringBuilds: true
-  }
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig 
